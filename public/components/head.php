e2a2910170bb714c73f0fac57adc0c54d4a8ccca
Grau <jevgm.03@gmail.com>
2025-07-03 01:40:24 -0400

    Se agregó icono al sistema

--- public/components/head.php
+++ public/components/head.php
@@ -1,6 +1,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="icon" href="public/assets/img/logo.png" type="image/png">
+
 <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="public/css/styles.css">
 
